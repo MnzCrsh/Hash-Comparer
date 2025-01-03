9cb0c30cedadd0ed4233bf8f6a8bc35556a4febb
MnzCrsh <96451040+MnzCrsh@users.noreply.github.com>
2025-01-03 21:14:35 +0300

    add to startup

--- FileHashComparer/Program.cs
+++ FileHashComparer/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using FileHashComparer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+var builder = new ServiceCollection();
+
+
+builder.AddLogging(config =>
+{
+    var logger = Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .MinimumLevel.Debug()
+        .CreateLogger();
+    
+    config.ClearProviders();
+    config.AddSerilog(logger, true);
+});
+builder.AddSingleton<RecursiveFileComparer>();
+
+var serviceProvider = builder.BuildServiceProvider();
+
+var comparer = serviceProvider.GetRequiredService<RecursiveFileComparer>();
+
+Console.WriteLine("Input path to directory");
+var startingDirectory = Console.ReadLine();
+
+using var cts = new CancellationTokenSource();
+var token = cts.Token;
+
+try
+{
+    var duplicateFiles = await comparer.SearchDuplicateFilesAsync(startingDirectory!, token);
+    Console.WriteLine("Found duplicate files:");
+
+    foreach (var file in duplicateFiles)
+    {
+        Console.WriteLine(file);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    Console.ReadLine();
+    throw;
+}
